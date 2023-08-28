b7832bc24f61ef007a853d40091a72419d93b372
Vbrawl <greninjagr@gmail.com>
2023-08-28 12:42:54 +0300

    Add a file to load every part of the plugin.

---
+++ main.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once("relocation.php");
+
+require_once(DATABASE_ADAPTER_PATH."/src/database.php");
+require_once(DATABASE_ADAPTER_PATH."/src/result.php");
+require_once(DATABASE_ADAPTER_PATH."/src/adapter.php");
